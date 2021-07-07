abe3b328c2cc9868a963aa52053deafdd6ad1fdc
EzrealZoe <1339252583@qq.com>
2021-07-07 09:54:10 +0800

    修改为相对路径

--- src/pages/Users/register.vue
+++ src/pages/Users/register.vue
@@ -439,7 +439,7 @@
         position: relative;
         width: 100%;
         height: 100%;
-        background-image: url(E:/fore/src/assets/images/3.jpg);
+        background-image: url(../../assets/images/3.jpg);
         background-size: 100%;
         background-repeat: no-repeat;
         min-width: 1325px;
